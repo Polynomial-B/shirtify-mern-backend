6595753f0f00201c905b43914787485a1539efe6
Matt <mphlamb@gmail.com>
2024-06-21 11:09:04 +0200

    deployment functions added

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,15 +6,22 @@ import { connectToDb } from './db/helpers.js';
 import errorHandler from './middleware/errorHandling.js';
 import express from 'express';
 import { fileURLToPath } from 'url';
-import { port } from './config/environment.js';
-import shirtRouter from './controllers/shirt.js';
-import authRouter from './controllers/auth.js';
-import wishlistRouter from './controllers/wishlist.js';
+import { dbURL, port } from '../../config/environment.js';
+import shirtRouter from '../../controllers/shirt.js';
+import authRouter from '../../controllers/auth.js';
+import wishlistRouter from '../../controllers/wishlist.js';
+
+import cors from 'cors'
+import serverless from "serverless-http"
+
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/auth', authRouter)
+
+app.use(cors());
+
+app.use('/api/auth', authRouter);
 app.use('/api/shirts', shirtRouter);
 app.use('/api/wishlist', wishlistRouter);
 app.use(errorHandler);
@@ -31,7 +38,6 @@ async function startServer() {
   try {
     await connectToDb();
     console.log('Database connected.');
-    app.listen(port, () => console.log(`Listening on Port: ${port}`));
   } catch (err) {
     console.log('Failed to connect to database.');
     console.log(err);
@@ -39,3 +45,5 @@ async function startServer() {
 }
 
 startServer();
+
+export const handler = serverless(app)
