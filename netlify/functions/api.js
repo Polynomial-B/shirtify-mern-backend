3147762f46012e9ecc639832bfd6cd1251a50271
Matt <mphlamb@gmail.com>
2024-06-21 11:23:10 +0200

    deployment fixes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,9 @@
 import 'dotenv/config';
 
-import path, { dirname } from 'path';
 
 import { connectToDb } from './db/helpers.js';
 import errorHandler from './middleware/errorHandling.js';
 import express from 'express';
-import { fileURLToPath } from 'url';
-import { dbURL, port } from '../../config/environment.js';
 import shirtRouter from '../../controllers/shirt.js';
 import authRouter from '../../controllers/auth.js';
 import wishlistRouter from '../../controllers/wishlist.js';
@@ -26,13 +23,6 @@ app.use('/api/shirts', shirtRouter);
 app.use('/api/wishlist', wishlistRouter);
 app.use(errorHandler);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.get('/', function (_req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-
-});
 
 async function startServer() {
   try {
